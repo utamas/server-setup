869f2b2c45ed3ea4efed8b72cff26fc832d2568d
Tamas Utasi <tamas.utasi@gmail.com>
2016-03-07 12:48:59 +0100

    create-box stript is creating hawk1222 image, that can be used in subsequent steps as a base.

--- steps/01_create_basic_vagrant_image/create-box.sh
+++ steps/01_create_basic_vagrant_image/create-box.sh
@@ -3,19 +3,28 @@
 
 # vboxmanage list vms
 function createBox() {
-	local logFile=/tmp/kernel-update.log
+	local -r logFile=/tmp/kernel-update.log
+	local -r boxname=hawk1222
+
+	echo "You can track progress by 'tail -f $logFile'"
 
 	echo "" > $logFile
 	echo "Deleting VM"
 	vagrant destroy --force >> $logFile
 	echo "Provisioning VM"
 	vagrant up --provision >> $logFile
-	echo "Restarting VM"
-	vagrant reload  >> $logFile
+	#echo "Restarting VM"
+	#vagrant reload  >> $logFile
 	echo "Shutting down VM"
 	vagrant halt >> $logFile
-	#echo "Creating box"
-	#vagrant package --base hawk1222 --output hawk1222.box
+	echo "Creating box"
+	rm -rf $boxname.box
+	vagrant package --output $boxname.box >> $logFile
+	echo "Importing box"
+	if [ -n "$(vagrant box list | grep $boxname)" ]; then
+		vagrant box remove $boxname >> $logFile
+	fi
+	vagrant box add $boxname $boxname.box >> $logFile
 }
 
 createBox "$@"
