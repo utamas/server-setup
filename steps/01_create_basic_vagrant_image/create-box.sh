1a3c78f9a3e79b6d86663251147505da1553b978
Tamas Utasi <tamas.utasi@gmail.com>
2016-03-16 16:13:17 +0100

    Formatted create-box.sh

--- steps/01_create_basic_vagrant_image/create-box.sh
+++ steps/01_create_basic_vagrant_image/create-box.sh
@@ -3,23 +3,26 @@
 
 # vboxmanage list vms
 function createBox() {
+	local -r boxname=${1:-'hawk1222'}; shift
+
 	local -r logFile=/tmp/kernel-update.log
-	local -r boxname=hawk1222
 
 	echo "You can track progress by 'tail -f $logFile'"
-
 	echo "" > $logFile
+
 	echo "Deleting VM"
 	vagrant destroy --force >> $logFile
+
 	echo "Provisioning VM"
 	vagrant up --provision >> $logFile
-	#echo "Restarting VM"
-	#vagrant reload  >> $logFile
+
 	echo "Shutting down VM"
 	vagrant halt >> $logFile
+
 	echo "Creating box"
 	rm -rf $boxname.box
 	vagrant package --output $boxname.box >> $logFile
+
 	echo "Importing box"
 	if [ -n "$(vagrant box list | grep $boxname)" ]; then
 		vagrant box remove $boxname >> $logFile
